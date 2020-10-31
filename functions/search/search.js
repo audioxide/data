2f5eac8509fa7ba63d437c100766372832da4f78
andrew.bridge <andrew.bridge@derivitec.com>
2020-10-31 23:06:14 +0000

    Restrict origin dynamically

--- functions/search/search.js
+++ functions/search/search.js
@@ -3,6 +3,13 @@ const path = require('path');
 const FlexSearch = require('flexsearch');
 const read = (filename) => fs.readFileSync(path.resolve(__dirname, filename), { encoding: 'utf8' });
 
+const netlifyDomainSuffix = "competent-ardinghelli-0dde0e.netlify.app";
+const domains = [
+  "https://alpha.audioxide.com",
+  "https://beta.audioxide.com",
+  "https://audioxide.com",
+];
+
 const index = new FlexSearch(JSON.parse(read('./searchOptions.json')));
 index.import(read('./searchIndex.json'));
 
@@ -13,6 +20,14 @@ const limitOpt = { limit: LIMIT };
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
+  const { headers: { origin } } = event;
+  if (!domains.includes(origin) && !origin.endsWith(netlifyDomainSuffix)) {
+    return {
+      statusCode: 401,
+      body: "{}",
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "https://audioxide.com" },
+    };
+  }
   try {
     const term = event.queryStringParameters.term.toLowerCase();
     const results = new Set();
@@ -35,7 +50,7 @@ exports.handler = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ event, response }),
+      body: JSON.stringify(response),
         /* .slice(0, 10)
         .map(route => lookup[route])
         .sort((a, b) => {
@@ -45,7 +60,7 @@ exports.handler = async (event, context) => {
           return ((matchB > matchA) * 2) -1;
         }) ),*/
       // // more keys you can return:
-      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": origin },
       // isBase64Encoded: true,
     }
   } catch (err) {
