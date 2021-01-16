c6f4b9268475578d59e8f796df75937700cab84b
Andrew Bridge <andrew@andrewhbridge.co.uk>
2021-01-16 13:42:33 +0000

    Use node-fetch to trigger build hook

--- plugins/netlify-plugin-trigger-website-rebuild/index.js
+++ plugins/netlify-plugin-trigger-website-rebuild/index.js
@@ -1,13 +1,14 @@
-const https = require('https');
+const fetch = require('node-fetch');
 
 module.exports = {
   onPostBuild() {
-    if (process.env.BRANCH !== 'main') return;
-    return https.request({
-      hostname: 'https://api.netlify.com',
-      port: 443,
-      path: '/build_hooks/' + process.env.WEBSITE_DEPLOY_HOOK,
-      method: 'POST'
-    }).end();
+    if (process.env.BRANCH !== 'main') return Promise.resolve();
+    return fetch(
+      'https://api.netlify.com/build_hooks/' + process.env.WEBSITE_DEPLOY_HOOK,
+      { method: 'POST' },
+    ).then(r => {
+      if (!r.ok) throw Error(`Error ${r.status}: ${r.statusText}`);
+      return r.ok;
+    });
   }
 }
