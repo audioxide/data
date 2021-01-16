a555c54796d7410f30e4fddda8a366583fedc3ba
Andrew Bridge <andrew@andrewhbridge.co.uk>
2021-01-16 00:06:21 +0000

    Add conditional website rebuild trigger (#29)
    * Add conditional website rebuild trigger
    
    * Correct the branch name exception

---
+++ plugins/netlify-plugin-trigger-website-rebuild/index.js
@@ -0,0 +1,13 @@
+const https = require('https');
+
+module.exports = {
+  onPostBuild() {
+    if (process.env.BRANCH !== 'main') return;
+    return https.request({
+      hostname: 'https://api.netlify.com',
+      port: 443,
+      path: '/build_hooks/' + process.env.WEBSITE_DEPLOY_HOOK,
+      method: 'POST'
+    }).end();
+  }
+}
